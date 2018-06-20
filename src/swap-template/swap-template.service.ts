65e31719544fea822b4b8efb3e3c52e06446c276
Volodymyr Matskiv <volodymyr.matskiv@thegameagency.com>
2018-06-20 13:11:16 +0300

    Added try catch/ changed code style

--- src/swap-template/swap-template.service.ts
+++ src/swap-template/swap-template.service.ts
@@ -19,29 +19,34 @@ export class SwapTemplateService {
 
   async create(id: string, onchainAsset: string, onchainAccount: string, offchainAsset: string, offchainAccount: string, rate: string, chain: Chain): Promise<void> {
     const aerumAccounts = await this.web3.eth.getAccounts();
-    return this.swapTemplateContract.methods.register(id
-      , onchainAsset
-      , onchainAccount
-      , offchainAsset
-      , offchainAccount
-      , rate
-      , chain).send({from: aerumAccounts[process.env.privateAerNodeAddressIndex]});
+    return this.swapTemplateContract.methods.register(id,
+      onchainAsset,
+      onchainAccount,
+      offchainAsset,
+      offchainAccount,
+      rate,
+      chain).send({from: aerumAccounts[process.env.privateAerNodeAddressIndex]});
   }
 
   async findById(id: string): Promise<SwapTemplate> {
     const aerumAccounts = await this.web3.eth.getAccounts();
-    const result = await this.swapTemplateContract.methods.templateById(id).call({from: aerumAccounts[process.env.privateAerNodeAddressIndex]});
-    const template: SwapTemplate = {
-      id: result[0],
-      owner: result[1],
-      onchainAsset: result[2],
-      onchainAccount: result[3],
-      offchainAsset: result[4],
-      offchainAccount: result[5],
-      rate: Number(result[6]),
-      chain: result[7],
-      state: ''
+    try{
+      const result = await this.swapTemplateContract.methods.templateById(id).call({from: aerumAccounts[process.env.privateAerNodeAddressIndex]});
+      const template: SwapTemplate = {
+        id: result[0],
+        owner: result[1],
+        onchainAsset: result[2],
+        onchainAccount: result[3],
+        offchainAsset: result[4],
+        offchainAccount: result[5],
+        rate: Number(result[6]),
+        chain: result[7],
+        state: ''
+      }
+      return template;
+    }catch(err){
+      console.log('template load error', err);
+      throw err;
     }
-    return template;
   }
 }
